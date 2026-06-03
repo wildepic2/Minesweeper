0cfc2a8cc2e3f2caca3c632ed232503305229e1a
Botond Payerhofer <138814330+wildepic2@users.noreply.github.com>
2026-06-03 08:27:51 +0200

    Remove reset and generate functions on game over
    Removed functions related to game over state.

--- src/main.c
+++ src/main.c
@@ -131,9 +131,6 @@ int main(void) {
                 drawField();
                 break;
             case GAME_OVER:
-                resetGrass();
-                generateBombs();
-                calculateNumbers();
                 //endGame();
                 break;
         }
